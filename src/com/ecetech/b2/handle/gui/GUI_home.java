de698fe18587472d5c5d332e681d02b43248b7de
GreyD <GreyD@DESKTOP-CO2L0AQ>
2020-01-09 18:02:56 +0100

    modification des noms des panels

--- src/com/ecetech/b2/handle/gui/GUI_home.java
+++ src/com/ecetech/b2/handle/gui/GUI_home.java
@@ -76,68 +76,68 @@ public class GUI_home {
 		panel.add(layeredPane);
 		
 		// declaration panel qui ne bouge pas gauche menu et en haut barre de recherche
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(102, 205, 170));
-		panel_1.setForeground(new Color(0, 0, 0));
-		panel_1.setBounds(258, 0, 1005, 103);
-		panel.add(panel_1);
-		panel_1.setLayout(null);
+		JPanel barre_recherche = new JPanel();
+		barre_recherche.setBackground(new Color(102, 205, 170));
+		barre_recherche.setForeground(new Color(0, 0, 0));
+		barre_recherche.setBounds(258, 0, 1005, 103);
+		panel.add(barre_recherche);
+		barre_recherche.setLayout(null);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setForeground(new Color(0, 0, 0));
-		panel_2.setBackground(new Color(32, 178, 170));
-		panel_2.setBounds(0, 0, 259, 645);
-		panel.add(panel_2);
-		panel_2.setLayout(null);
+		JPanel barre_Menu = new JPanel();
+		barre_Menu.setForeground(new Color(0, 0, 0));
+		barre_Menu.setBackground(new Color(32, 178, 170));
+		barre_Menu.setBounds(0, 0, 259, 645);
+		panel.add(barre_Menu);
+		barre_Menu.setLayout(null);
 		
 		// declaration autre panel 
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(102, 255, 153));
-		panel_3.setBounds(0, 0, 1005, 546);
-		layeredPane.add(panel_3);
-		panel_3.setLayout(null);
+		JPanel accueil = new JPanel();
+		accueil.setBackground(new Color(102, 255, 153));
+		accueil.setBounds(0, 0, 1005, 546);
+		layeredPane.add(accueil);
+		accueil.setLayout(null);
 		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBackground(new Color(102, 255, 153));
-		panel_4.setForeground(new Color(153, 255, 153));
-		panel_4.setBounds(0, 0, 1005, 546);
-		layeredPane.add(panel_4);
-		panel_4.setLayout(null);
+		JPanel profil = new JPanel();
+		profil.setBackground(new Color(102, 255, 153));
+		profil.setForeground(new Color(153, 255, 153));
+		profil.setBounds(0, 0, 1005, 546);
+		layeredPane.add(profil);
+		profil.setLayout(null);
 		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBackground(new Color(102, 255, 153));
-		panel_5.setBounds(0, 0, 1005, 546);
-		layeredPane.add(panel_5);
-		panel_5.setLayout(null);
+		JPanel examen = new JPanel();
+		examen.setBackground(new Color(102, 255, 153));
+		examen.setBounds(0, 0, 1005, 546);
+		layeredPane.add(examen);
+		examen.setLayout(null);
 		
-		JPanel panel_6 = new JPanel();
-		panel_6.setBounds(0, 0, 1005, 566);
-		layeredPane.add(panel_6);
+		JPanel diagnostic = new JPanel();
+		diagnostic.setBounds(0, 0, 1005, 566);
+		layeredPane.add(diagnostic);
 		
-		JPanel panel_7 = new JPanel();
-		panel_7.setBounds(0, 0, 1005, 566);
-		layeredPane.add(panel_7);
+		JPanel aide = new JPanel();
+		aide.setBounds(0, 0, 1005, 566);
+		layeredPane.add(aide);
 		
 		
 		//Barre de recherche
 		
 		textField = new JTextField();
 		textField.setBounds(888, 37, 107, 19);
-		panel_1.add(textField);
+		barre_recherche.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setIcon(new ImageIcon(GUI_home.class.getResource("/img/icons8_search_32px.png")));
 		lblNewLabel_2.setBounds(848, 0, 157, 97);
-		panel_1.add(lblNewLabel_2);
+		barre_recherche.add(lblNewLabel_2);
 		
 		
 		//Logo Handle
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(GUI_home.class.getResource("/img/logo handle.PNG")));
 		lblNewLabel.setBounds(58, 0, 122, 155);
-		panel_2.add(lblNewLabel);
+		barre_Menu.add(lblNewLabel);
 		
 		//label (zone de texte) Accueil
 		JLabel lblNewLabel_1 = new JLabel("Accueil");
@@ -145,7 +145,7 @@ public class GUI_home {
 		lblNewLabel_1.setForeground(Color.WHITE);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 32));
 		lblNewLabel_1.setBounds(42, 206, 167, 48);
-		panel_2.add(lblNewLabel_1);
+		barre_Menu.add(lblNewLabel_1);
 		
 		//label (zone de texte) Profil
 		JLabel lblProfil = new JLabel("Profil");
@@ -154,7 +154,7 @@ public class GUI_home {
 		lblProfil.setForeground(Color.WHITE);
 		lblProfil.setFont(new Font("Tahoma", Font.PLAIN, 32));
 		lblProfil.setBounds(42, 282, 167, 48);
-		panel_2.add(lblProfil);
+		barre_Menu.add(lblProfil);
 		
 		//label (zone de texte) Examen
 		JLabel lblExamen = new JLabel("Examen");
@@ -163,7 +163,7 @@ public class GUI_home {
 		lblExamen.setForeground(Color.WHITE);
 		lblExamen.setFont(new Font("Tahoma", Font.PLAIN, 32));
 		lblExamen.setBounds(42, 356, 178, 48);
-		panel_2.add(lblExamen);
+		barre_Menu.add(lblExamen);
 		
 		//label (zone de texte) Diagnostique
 		JLabel lblDiagnostique = new JLabel("Diagnostique");
@@ -172,7 +172,7 @@ public class GUI_home {
 		lblDiagnostique.setForeground(Color.WHITE);
 		lblDiagnostique.setFont(new Font("Tahoma", Font.PLAIN, 32));
 		lblDiagnostique.setBounds(10, 432, 268, 48);
-		panel_2.add(lblDiagnostique);
+		barre_Menu.add(lblDiagnostique);
 		
 		//label (zone de texte) Aide
 		JLabel lblAide = new JLabel("Aide");
@@ -181,7 +181,7 @@ public class GUI_home {
 		lblAide.setForeground(Color.WHITE);
 		lblAide.setFont(new Font("Tahoma", Font.PLAIN, 32));
 		lblAide.setBounds(42, 511, 178, 48);
-		panel_2.add(lblAide);
+		barre_Menu.add(lblAide);
 		
 		//boutton present en permanance panel 2 (panel sur le coté gauche)
 		
@@ -193,7 +193,7 @@ public class GUI_home {
 		accueilbutton.setOpaque(false);
 		accueilbutton.setContentAreaFilled(false);
 		accueilbutton.setBorderPainted(false);
-		panel_2.add(accueilbutton);
+		barre_Menu.add(accueilbutton);
 		
 		//bouton profil
 		JButton profilbutton = new JButton("");
@@ -203,7 +203,7 @@ public class GUI_home {
 		profilbutton.setBorderPainted(false);
 		profilbutton.setBackground(Color.WHITE);
 		profilbutton.setBounds(42, 282, 167, 44);
-		panel_2.add(profilbutton);
+		barre_Menu.add(profilbutton);
 		
 		//bouton examen
 		JButton examenbutton = new JButton("");
@@ -213,7 +213,7 @@ public class GUI_home {
 		examenbutton.setBorderPainted(false);
 		examenbutton.setBackground(Color.WHITE);
 		examenbutton.setBounds(42, 356, 167, 44);
-		panel_2.add(examenbutton);
+		barre_Menu.add(examenbutton);
 		
 		//boutton diagnostique
 		JButton diagnostiquebutton = new JButton("");
@@ -223,7 +223,7 @@ public class GUI_home {
 		diagnostiquebutton.setBorderPainted(false);
 		diagnostiquebutton.setBackground(Color.WHITE);
 		diagnostiquebutton.setBounds(13, 432, 236, 44);
-		panel_2.add(diagnostiquebutton);
+		barre_Menu.add(diagnostiquebutton);
 		
 		//bouton aide
 		JButton aidebutton = new JButton("");
@@ -233,111 +233,111 @@ public class GUI_home {
 		aidebutton.setBorderPainted(false);
 		aidebutton.setBackground(Color.WHITE);
 		aidebutton.setBounds(42, 515, 136, 44);
-		panel_2.add(aidebutton);
+		barre_Menu.add(aidebutton);
 		
 		
-		// label panel examen panel_5
+		// label panel examen examen
 		JLabel lblExamen_1 = new JLabel("EXAMEN");
 		lblExamen_1.setForeground(new Color(0, 51, 51));
 		lblExamen_1.setFont(new Font("Tahoma", Font.BOLD, 40));
 		lblExamen_1.setBounds(440, 36, 185, 47);
-		panel_5.add(lblExamen_1);
+		examen.add(lblExamen_1);
 		
-		// label panel profil panel_4
+		// label panel profil profil
 		JLabel lblProfil_1 = new JLabel("PROFIL");
 		lblProfil_1.setForeground(new Color(0, 51, 51));
 		lblProfil_1.setFont(new Font("Tahoma", Font.BOLD, 40));
 		lblProfil_1.setBounds(429, 28, 185, 47);
-		panel_4.add(lblProfil_1);
+		profil.add(lblProfil_1);
 		
 		JLabel lblNewLabel_5 = new JLabel("IDENTIFIANT");
 		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
 		lblNewLabel_5.setBounds(157, 111, 141, 47);
-		panel_4.add(lblNewLabel_5);
+		profil.add(lblNewLabel_5);
 		
 		textField_1 = new JTextField();
 		textField_1.setBounds(157, 157, 194, 19);
-		panel_4.add(textField_1);
+		profil.add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
 		textField_2.setBounds(157, 232, 194, 19);
-		panel_4.add(textField_2);
+		profil.add(textField_2);
 		
 		JLabel lblMotDePasse = new JLabel("MOT DE PASSE");
 		lblMotDePasse.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
 		lblMotDePasse.setBounds(157, 186, 162, 47);
-		panel_4.add(lblMotDePasse);
+		profil.add(lblMotDePasse);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
 		textField_3.setBounds(157, 307, 194, 19);
-		panel_4.add(textField_3);
+		profil.add(textField_3);
 		
 		JLabel lblPrenom = new JLabel("EMAIL");
 		lblPrenom.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
 		lblPrenom.setBounds(157, 261, 141, 47);
-		panel_4.add(lblPrenom);
+		profil.add(lblPrenom);
 		
 		JLabel lblAge_1 = new JLabel("AGE");
 		lblAge_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
 		lblAge_1.setBounds(587, 261, 141, 47);
-		panel_4.add(lblAge_1);
+		profil.add(lblAge_1);
 		
 		JLabel lblAge = new JLabel("PRENOM");
 		lblAge.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
 		lblAge.setBounds(587, 111, 141, 47);
-		panel_4.add(lblAge);
+		profil.add(lblAge);
 		
 		JLabel lblProfession = new JLabel("NOM");
 		lblProfession.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
 		lblProfession.setBounds(587, 186, 162, 47);
-		panel_4.add(lblProfession);
+		profil.add(lblProfession);
 		
 		JLabel lblProfession_1 = new JLabel("PROFESSION");
 		lblProfession_1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 20));
 		lblProfession_1.setBounds(587, 336, 141, 47);
-		panel_4.add(lblProfession_1);
+		profil.add(lblProfession_1);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
 		textField_4.setBounds(587, 157, 194, 19);
-		panel_4.add(textField_4);
+		profil.add(textField_4);
 		
 		textField_5 = new JTextField();
 		textField_5.setColumns(10);
 		textField_5.setBounds(587, 232, 194, 19);
-		panel_4.add(textField_5);
+		profil.add(textField_5);
 		
 		textField_6 = new JTextField();
 		textField_6.setColumns(10);
 		textField_6.setBounds(587, 307, 194, 19);
-		panel_4.add(textField_6);
+		profil.add(textField_6);
 		
 		textField_7 = new JTextField();
 		textField_7.setColumns(10);
 		textField_7.setBounds(587, 382, 194, 19);
-		panel_4.add(textField_7);
+		profil.add(textField_7);
 		
-		// label panel accueil panel_3
+		// label panel accueil accueil
 		JLabel lblNewLabel_3 = new JLabel("BIENVENUE");
 		lblNewLabel_3.setForeground(new Color(0, 51, 51));
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 40));
 		lblNewLabel_3.setBounds(417, 29, 252, 47);
-		panel_3.add(lblNewLabel_3);
+		accueil.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("");
 		lblNewLabel_4.setIcon(new ImageIcon(GUI_home.class.getResource("/img/corpsbioflash.png")));
 		lblNewLabel_4.setBounds(451, 109, 160, 362);
-		panel_3.add(lblNewLabel_4);
+		accueil.add(lblNewLabel_4);
 		
 		
 		//action des bouttons present dans le panel 2 permettant la redirection vers les autres panels
 		accueilbutton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				layeredPane.removeAll();
-				layeredPane.add(panel_3);
+				layeredPane.add(accueil);
 				layeredPane.repaint();
 				layeredPane.revalidate();
 			}
@@ -345,7 +345,7 @@ public class GUI_home {
 		profilbutton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				layeredPane.removeAll();
-				layeredPane.add(panel_4);
+				layeredPane.add(profil);
 				layeredPane.repaint();
 				layeredPane.revalidate();
 			}
@@ -353,7 +353,7 @@ public class GUI_home {
 		examenbutton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				layeredPane.removeAll();
-				layeredPane.add(panel_5);
+				layeredPane.add(examen);
 				layeredPane.repaint();
 				layeredPane.revalidate();
 			}
@@ -361,7 +361,7 @@ public class GUI_home {
 		diagnostiquebutton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				layeredPane.removeAll();
-				layeredPane.add(panel_6);
+				layeredPane.add(diagnostic);
 				layeredPane.repaint();
 				layeredPane.revalidate();
 			}
@@ -369,7 +369,7 @@ public class GUI_home {
 		aidebutton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				layeredPane.removeAll();
-				layeredPane.add(panel_7);
+				layeredPane.add(aide);
 				layeredPane.repaint();
 				layeredPane.revalidate();
 			}
