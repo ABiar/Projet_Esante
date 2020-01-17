53aaa7eb742a575437a7f8a7821027c3895795ee
GreyD <GreyD@DESKTOP-MFH8SGN>
2020-01-17 10:55:56 +0100

    ajoût examen

---
+++ src/com/ecetech/b2/handle/gui/Exam.java
@@ -0,0 +1,52 @@
+package com.ecetech.b2.handle.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+
+public class Exam extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Exam frame = new Exam();
+					frame.setUndecorated(true);
+					frame.setVisible(true);
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Exam() {
+		setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 894, 482);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel label = new JLabel("");
+		label.setBackground(Color.ORANGE);
+		label.setBounds(0, 0, 878, 41);
+		contentPane.add(label);
+	}
+}
