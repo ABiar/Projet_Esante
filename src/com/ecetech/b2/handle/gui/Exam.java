492f7649fd184c661fe0618f204fe95383651ae0
GreyD <GreyD@DESKTOP-CO2L0AQ>
2020-01-21 16:39:05 +0100

    Ajout de déplacement de fenêtre et exit btn dans exam

--- src/com/ecetech/b2/handle/gui/Exam.java
+++ src/com/ecetech/b2/handle/gui/Exam.java
@@ -8,10 +8,16 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class Exam extends JFrame {
 
 	private JPanel contentPane;
+	int xx,xy;
 
 	/**
 	 * Launch the application.
@@ -21,7 +27,7 @@ public class Exam extends JFrame {
 			public void run() {
 				try {
 					Exam frame = new Exam();
-					frame.setUndecorated(true);
+					frame.setUndecorated(true); // Sert à enlever les bordures.
 					frame.setVisible(true);
 					
 				} catch (Exception e) {
@@ -35,18 +41,45 @@ public class Exam extends JFrame {
 	 * Create the frame.
 	 */
 	public Exam() {
+		
 		setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 894, 482);
+		setBounds(100, 100, 878, 482);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.WHITE);
+		contentPane.setBackground(Color.GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel label = new JLabel("");
-		label.setBackground(Color.ORANGE);
-		label.setBounds(0, 0, 878, 41);
-		contentPane.add(label);
+		JPanel panel = new JPanel();
+			panel.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mousePressed(MouseEvent e) {
+					xx = e.getX();
+					xy = e.getY();
+				}
+			});
+			panel.addMouseMotionListener(new MouseMotionAdapter() {
+				@Override
+				public void mouseDragged(MouseEvent f) {
+					int x = f.getXOnScreen();
+					int y = f.getYOnScreen();
+					Exam.this.setLocation(x - xx,y -  xy);
+				}
+			});
+		panel.setBounds(0, 0, 878, 64);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("X");
+		lblNewLabel.addMouseListener(new MouseAdapter() {
+			
+			public void mouseClicked(MouseEvent e) {
+				System.exit(0);
+			}
+		});
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblNewLabel.setBounds(840, 11, 13, 27);
+		panel.add(lblNewLabel);
 	}
 }
