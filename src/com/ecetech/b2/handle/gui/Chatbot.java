a6eda01791caae17e341d1b46a3729c58466b977
elian <elian@DESKTOP-JM4A34U>
2020-03-12 13:13:11 +0100

    fini

--- src/com/ecetech/b2/handle/gui/Chatbot.java
+++ src/com/ecetech/b2/handle/gui/Chatbot.java
@@ -26,9 +26,11 @@ import javax.swing.JTextArea;
 public class Chatbot extends JFrame {
 
 	private JPanel contentPane;
-	int xx,xy;
+	int xx, xy;
 	private JTextField field;
 	private String question1;
+	private static int level = 0;
+	private static Chatbot frame;
 
 	/**
 	 * Launch the application.
@@ -37,7 +39,7 @@ public class Chatbot extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Chatbot frame = new Chatbot();
+					frame = new Chatbot();
 					frame.setUndecorated(true);
 					frame.setVisible(true);
 					frame.setLocationRelativeTo(null);
@@ -58,7 +60,7 @@ public class Chatbot extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel north = new JPanel();
 		north.addMouseListener(new MouseAdapter() {
 			@Override
@@ -72,14 +74,14 @@ public class Chatbot extends JFrame {
 			public void mouseDragged(MouseEvent f) {
 				int x = f.getXOnScreen();
 				int y = f.getYOnScreen();
-				Chatbot.this.setLocation(x - xx,y -  xy);
+				Chatbot.this.setLocation(x - xx, y - xy);
 			}
 		});
 		north.setBackground(new Color(0, 128, 128));
 		north.setBounds(0, 0, 640, 65);
 		contentPane.add(north);
 		north.setLayout(null);
-		
+
 		JButton fermer = new JButton("");//
 		fermer.setBackground(new Color(255, 165, 0));
 		fermer.setIcon(new ImageIcon(Chatbot.class.getResource("/img/close.png")));
@@ -88,7 +90,7 @@ public class Chatbot extends JFrame {
 		fermer.setOpaque(false);
 		fermer.setBorderPainted(false);
 		north.add(fermer);
-		
+
 		JButton Reduire = new JButton("");
 		Reduire.setIcon(new ImageIcon(Chatbot.class.getResource("/img/icons8-fl\u00E8che-r\u00E9duire-48.png")));
 		Reduire.setBounds(477, 10, 40, 45);
@@ -96,83 +98,97 @@ public class Chatbot extends JFrame {
 		Reduire.setOpaque(false);
 		Reduire.setBorderPainted(false);
 		north.add(Reduire);
-		
+
 		JLabel lblNewLabel = new JLabel("ChatBot");
 		lblNewLabel.setForeground(SystemColor.text);
 		lblNewLabel.setBounds(10, 0, 123, 65);
 		north.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		
+
 		field = new JTextField();
 		field.setBounds(28, 400, 411, 36);
 		contentPane.add(field);
 		field.setColumns(10);
-		
+
 		JLabel image = new JLabel("");
 		image.setIcon(new ImageIcon(Chatbot.class.getResource("/img/corpsbioflash.png")));
 		image.setBounds(449, 75, 154, 361);
 		contentPane.add(image);
-		
+
 		JTextArea txt = new JTextArea();
 		txt.setBounds(10, 0, 411, 210);
 		contentPane.add(txt);
-		txt.append("Alvi= Pouvez vous réaliser ce mouvement ?\n"); 
-		
-		JScrollPane pane = new JScrollPane(txt, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		txt.append("Alvi= Pouvez vous réaliser ce mouvement ?\n");
+
+		JScrollPane pane = new JScrollPane(txt, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		pane.setBounds(28, 126, 411, 210);
 		getContentPane().add(pane);
-		
+
 		field.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				level = level + 1; // mise à jour au fur et à mesure
 				String reponse = field.getText();
-				txt.append("You= "+reponse+"\n");
-				field.setText("");
-				if(reponse.toLowerCase().equals("oui")) {
-					txt.append("Alvi = Observez vous la présence d'un Oedeme de ce type ?\n");
-					image.setIcon(new ImageIcon(GUI_home.class.getResource("/img/close.png")));
-					String reponse1 = field.getText();
-					field.setText("");
-					if(reponse1.toLowerCase().equals("oui")) {
+				switch (level) {
+				case 1:
+					if (reponse.toLowerCase().equals("oui")) {
+						txt.append("Alvi = Observez vous la présence d'un Oedeme de ce type ?\n");
+						image.setIcon(new ImageIcon(GUI_home.class.getResource("/img/close.png")));
+					} else if (reponse.toLowerCase().equals("non")) {
+						txt.append("Alvi = Mince ! Nous ne pouvons pas vous aider \n");
+						frame.dispose();
+						try {
+							GUI_home window = new GUI_home();
+							window.framebis.setVisible(true);
+						} catch (Exception exp) {
+							exp.printStackTrace();
+						}
+					}
+					break;
+				case 2:
+					if (reponse.toLowerCase().equals("oui")) {
 						txt.append("Alvi = Observez vous la présence d'un de ces SYmpthomes\n");
-					}
-					else if(reponse1.toLowerCase().equals("non")) {
+					} else if (reponse.toLowerCase().equals("non")) {
 						txt.append("Alvi = Mince ! Et ce mouvement la pouvez vous le faire ? \n");
-						mouvement1 f = new mouvement1();
-						f.setUndecorated(true);
-						f.setVisible(true);
+						level++;
 					}
+					break;
+				// Case ou en deux il répond oui
+				case 3:
+					break;
+				// Case ou en deux il répond non
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;
+				case 7:
+					break;
+				case 8:
+					break;
 				}
-				else if(reponse.toLowerCase().equals("non")) {
-					txt.append("Bot = Mince ! Nous ne pouvons pas vous aider \n");
-				}
-				else {
-					txt.append("Bot = Je ne vous ai pas compris \n");
-					txt.append("Bot = Pouvez vous réaliser ce mouvement? \n");
-				}
-				
 			}
 		});
-		
-		
+
 		JLabel lblQuestion = new JLabel("Question");
 		lblQuestion.setForeground(SystemColor.textHighlight);
 		lblQuestion.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		lblQuestion.setBounds(28, 64, 411, 52);
 		contentPane.add(lblQuestion);
-		
+
 		JLabel lblVotreRponse = new JLabel("Votre R\u00E9ponse");
 		lblVotreRponse.setForeground(SystemColor.textHighlight);
 		lblVotreRponse.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		lblVotreRponse.setBounds(27, 346, 411, 52);
 		contentPane.add(lblVotreRponse);
-		
-		
+
 		fermer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Chatbot.this.dispose();
 			}
 		});
-		
+
 		Reduire.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setState(Chatbot.ICONIFIED);
